2e84fad06b22177dd0a4c84471f0c0f5c581d1b0
Daniel Krech <eikeon@eikeon.com>
2012-05-14 21:52:04 -0400

    switch to using bytes.Buffer instead of MMBuffer

--- largeBuffer.go
+++ largeBuffer.go
@@ -1,121 +1,12 @@
 package funnelsort
 
-// A mmap backed buffer for large buffers
-
 import (
+	"bytes"
 	"io"
-	"io/ioutil"
-	"os"
-	"syscall"
 )
 
-type MMBuffer struct {
-	file *os.File
-	buf  []byte
-	off  int
-	mmap []byte
-	mapped bool
-}
-
-func (b *MMBuffer) Len() int { return len(b.buf) - b.off }
-
-func (b *MMBuffer) Reset() {
-	b.off = 0
-	b.buf = b.buf[0:0]
-}
-
-// grow grows the buffer to guarantee space for n more bytes.
-// It returns the index where bytes should be written.
-func (b *MMBuffer) grow(n int) int {
-	m := b.Len()
-	// If buffer is empty, reset to recover space.
-	if m == 0 && b.off != 0 {
-		b.Reset()
-	}
-	if len(b.buf)+n > cap(b.buf) {
-		b.buf = b.Map(2*cap(b.buf) + n)[0:0]
-		b.off = 0
-	}
-	b.buf = b.buf[0 : b.off+m+n]
-	return b.off + m
-}
-
-func (b *MMBuffer) Write(p []byte) (n int, err error) {
-	m := b.grow(len(p))
-	return copy(b.buf[m:], p), nil
-}
-
-func (b *MMBuffer) Read(p []byte) (n int, err error) {
-	if b.mapped == false {
-		b.Map(len(b.buf))
-	}
-	if b.off >= len(b.buf) {
-		// MMBuffer is empty, reset to recover space.
-		b.Reset()
-		if len(p) == 0 {
-			return
-		}
-		return 0, io.EOF
-	}
-	n = copy(p, b.buf[b.off:])
-	b.off += n
-	return
-}
-
-func (b *MMBuffer) unmap() {
-	if len(b.mmap) > 0 {
-		err := syscall.Munmap(b.mmap)
-		if err != nil {
-			panic(err)
-		}
-		b.mmap = []byte{}
-		b.mapped = false
-	}
-}
-
-func (b *MMBuffer) Close() {
-	b.unmap()
-	b.file.Close()
-	err := os.Remove(b.file.Name())
-	if err != nil {
-		//log.Println(err)
-	}
-}
-
-func (b *MMBuffer) Map(capacity int) []byte {
-	fi, err := b.file.Stat()
-	if err != nil {
-		panic(err)
-	}
-	if int64(capacity) > fi.Size() {
-		_, err = b.file.Seek(int64(capacity-1), 0)
-		if err != nil {
-			panic(err)
-		}
-		_, err = b.file.Write([]byte(" "))
-		if err != nil {
-			panic(err)
-		}
-	}
-	b.unmap()
-	mmap, err := syscall.Mmap(int(b.file.Fd()), 0, capacity, syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED)
-	if err != nil {
-		panic(err)
-	}
-	b.mapped = true
-	return mmap
-}
-
-func NewMMBuffer() *MMBuffer {
-	file, err := ioutil.TempFile("./", "tmpfunnelsort")
-	if err != nil {
-		panic(err)
-	}
-	return &MMBuffer{file: file}
-}
-
 type LargeBuffer struct {
-	buffers []*MMBuffer
+	buffers []*bytes.Buffer
 }
 
 func (mr *LargeBuffer) Read(p []byte) (n int, err error) {
@@ -130,32 +21,48 @@ func (mr *LargeBuffer) Read(p []byte) (n int, err error) {
 			}
 			return
 		}
-		current.Close()
 		mr.buffers = mr.buffers[1:]
 	}
 	return 0, io.EOF
 }
 
-const MAX_BUFFER_SIZE = (1 << 30)
+const MAX_BUFFER_SIZE = (1 << 14)
+
+func (mr *LargeBuffer) addBuffer() *bytes.Buffer {
+	current := bytes.NewBuffer(make([]byte, MAX_BUFFER_SIZE)[0:0])
+	mr.buffers = append(mr.buffers, current)
+	return current
+}
 
 func (mr *LargeBuffer) Write(p []byte) (n int, err error) {
+	if len(mr.buffers) == 0 {
+		mr.addBuffer()
+	}
 	current := mr.buffers[len(mr.buffers)-1]
-	if len(current.buf)+len(p) > MAX_BUFFER_SIZE {
-		current.unmap()
-		current = NewMMBuffer()
-		mr.buffers = append(mr.buffers, current)
+
+	if current.Len() >= MAX_BUFFER_SIZE {
+		current = mr.addBuffer()
+	}
+
+	if len(p) > MAX_BUFFER_SIZE {
+		written := 0
+		for written+MAX_BUFFER_SIZE < len(p) {
+			nn, _ := current.Write(p[written:written+MAX_BUFFER_SIZE])
+			written += nn
+			current = mr.addBuffer()
+		}
+		remaining := len(p) - written
+		current.Write(p[written:written+remaining])
+		return len(p), nil
 	}
 	return current.Write(p)
 }
 
 func (mr *LargeBuffer) Close() {
-	for _, b := range mr.buffers {
-		b.Close()
-	}
+	mr.buffers = nil
 }
 
 func NewLargeBuffer() *LargeBuffer {
 	lb := &LargeBuffer{}
-	lb.buffers = append(lb.buffers, NewMMBuffer())
 	return lb
 }
